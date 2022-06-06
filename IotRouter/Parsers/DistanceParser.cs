68ce320f0f2b61972fc3ac36f92e1893ba07457a
WimObiwan <wim@obiwan.be>
2022-06-06 20:03:38 +0200

    DistanceParser: Ignore missing temperature/humidity

--- IotRouter/Parsers/DistanceParser.cs
+++ IotRouter/Parsers/DistanceParser.cs
@@ -27,6 +27,19 @@ namespace IotRouter
                 new ParsedData.KeyValue("distance", distance),
             };
 
+            ParserValue humidityValue, temperatureValue;
+            if (parserData.TryGetPayloadValue("humid", out humidityValue)
+                && parserData.TryGetPayloadValue("temp", out temperatureValue))
+            {
+                decimal humidity = humidityValue.AsDecimal();
+                decimal temperature = temperatureValue.AsDecimal();
+                if (humidity != 0.0m && temperature != 0.0m)
+                {
+                    keyValues.Add(new ParsedData.KeyValue("humid", humidity));
+                    keyValues.Add(new ParsedData.KeyValue("temp", temperature));
+                }
+            }
+
             return new ParsedData(devEUI, dateTime, keyValues);
         }
     }
