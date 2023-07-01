2d9c8e3f6d952cf20d36f25d7ece2263ed4b86ec
WimObiwan <wim@obiwan.be>
2023-07-01 13:09:51 +0200

    Refactor LDDS75 parser

--- IotRouter/Parsers/Dragino/LDDS75.cs
+++ IotRouter/Parsers/Dragino/LDDS75.cs
@@ -4,28 +4,27 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace IotRouter.Parsers.Dragino
+namespace IotRouter.Parsers.Dragino;
+
+public class LDDS75 : Parser
 {
-    public class LDDS75 : Parser
+    public LDDS75(IServiceProvider serviceProvider, IConfigurationSection config, string name)
+        : base(serviceProvider.GetService<ILogger<LHT65>>(), name)
     {
-        public LDDS75(IServiceProvider serviceProvider, IConfigurationSection config, string name)
-            : base(serviceProvider.GetService<ILogger<LHT65>>(), name)
+    }
+
+    protected override ParsedData Parse(ParserData parserData)
+    {
+        string devEUI = parserData.GetDevEUI();
+        DateTime dateTime = parserData.GetTime();
+
+        var keyValues = new List<ParsedData.KeyValue>()
         {
-        }
+            new("batV", parserData.GetPayloadValue("batV").AsDecimal()),
+            new("distance", parserData.GetPayloadValue("distanceMm").AsDecimal()),
+            new("RSSI", parserData.GetRSSI()),                    
+        };
 
-        protected override ParsedData Parse(ParserData parserData)
-        {
-            string devEUI = parserData.GetDevEUI();
-            DateTime dateTime = parserData.GetTime();
-
-            var keyValues = new List<ParsedData.KeyValue>()
-            {
-                new ParsedData.KeyValue("batV", parserData.GetPayloadValue("batV").AsDecimal()),
-                new ParsedData.KeyValue("distance", parserData.GetPayloadValue("distanceMm").AsDecimal()),
-                new ParsedData.KeyValue("RSSI", parserData.GetRSSI()),                    
-            };
-
-            return new ParsedData(devEUI, dateTime, keyValues);
-        }
+        return new ParsedData(devEUI, dateTime, keyValues);
     }
 }
