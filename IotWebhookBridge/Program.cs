c2e8480b81c06606c7fef6ce23b52334b8acb003
WimObiwan <wim@obiwan.be>
2023-10-22 19:54:06 +0200

    UdpServer: Add for 1nce.iot

--- IotWebhookBridge/Program.cs
+++ IotWebhookBridge/Program.cs
@@ -3,15 +3,22 @@ using IotWebhookBridge;
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.Local.json", true);
 
-builder.Services.AddSingleton<IMqttService, MqttService>();
 builder.Services.AddOptions<MqttOptions>()
     .Bind(builder.Configuration.GetSection(MqttOptions.Position));
 builder.Services.AddOptions<WebhookOptions>()
     .Bind(builder.Configuration.GetSection(WebhookOptions.Position));
+builder.Services.AddOptions<UdpServerOptions>()
+    .Bind(builder.Configuration.GetSection(UdpServerOptions.Position));
+
+builder.Services.AddSingleton<IMqttService, MqttService>();
+builder.Services.AddSingleton<IUdpServerService, UdpServerService>();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.MapControllers();
+
+app.Services.GetRequiredService<IUdpServerService>().Initialize();
 
 app.Run();
